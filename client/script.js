a755766e1c0d0de38d511b60ad6e2b7b27da36c1
Nabi <artsanaat@gmail.com>
2023-01-28 12:35:03 +0300

    Added loader method.

---
+++ client/script.js
@@ -0,0 +1,21 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
