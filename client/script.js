9c61fa6473f9eb2f8a67eb017b07e2a371f801fd
Nabi <artsanaat@gmail.com>
2023-01-28 12:35:39 +0300

    Added type text method.

--- client/script.js
+++ client/script.js
@@ -19,3 +19,16 @@ function loader(element) {
         }
     }, 300);
 }
+
+function typeText(element, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+      if (index < text.length) {
+          element.innerHTML += text.charAt(index)
+          index++
+      } else {
+          clearInterval(interval)
+      }
+  }, 20)
+}
