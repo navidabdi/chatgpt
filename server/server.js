04f7db7565ccfde238e9d6fc2f777e2c3196516d
Nabi <artsanaat@gmail.com>
2023-01-28 12:56:56 +0300

    Added server script command.

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
     }
   })
   
-  app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+  app.listen(5001, () => console.log('AI server started on http://localhost:5001'))
