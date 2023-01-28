676fbae1542fb7cf2340cd67717b66dbb24ce7b0
Nabi <artsanaat@gmail.com>
2023-01-28 12:36:17 +0300

    Added generate unique id method.

--- client/script.js
+++ client/script.js
@@ -32,3 +32,14 @@ function typeText(element, text) {
       }
   }, 20)
 }
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
