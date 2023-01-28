546473de13e8aac57dd9e2b853c8544321202047
Nabi <artsanaat@gmail.com>
2023-01-28 12:48:03 +0300

    Added server API.

--- server/server.js
+++ server/server.js
@@ -8,3 +8,15 @@ dotenv.config()
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
