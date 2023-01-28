ae1815a5d7655cb403cfbe464f18011b01d2f8e2
Nabi <artsanaat@gmail.com>
2023-01-28 12:47:20 +0300

    Added server dependencies.

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
